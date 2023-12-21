bd8507d59c92c4d925fe0ef7294708b9e4ed86a9
gerasimovpavel <gerasimovpg@outlook.com>
2023-12-21 23:02:38 +0300

    Delete cmd/internal directory

--- cmd/internal/model/balance.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Balance struct {
-	Сurrent   float64 `json:"current" db:"current"`
-	WithDrawn float64 `json:"with_drawn" db:"with_drawn"`
-}
--- cmd/internal/model/withdraw.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "time"
-
-type Withdraw struct {
-	Order       string    `json:"order" db:"order"`
-	Sum         float64   `json:"sum" db:"summa"`
-	ProcessedAt time.Time `json:"processed_at" db:"processed_at"`
-}
